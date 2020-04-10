05cf8538b7828b8247f8e1de0c91a27bd2b1c802
GinaKastner <gina.kastner@stud.uni-regensburg.de>
2020-04-10 08:45:38 +0200

    Modified draft.

--- 04-Games/src/js/games/FallingStones.js
+++ 04-Games/src/js/games/FallingStones.js
@@ -4,10 +4,7 @@ import { GameObject, MovableGameObject } from "../GameObject.js";
 export class FallingStones extends GameTemplate {
 
     // TODO: 
-    // Fix bug: Player disappears when this.generateBullet() added.
-    // Movement directions correct?
-    // 2.4, 2.5: Delete bullets & stones.
-    // 2.6: Implement hit check (rectangle collision).  
+    // Fix bug: Player disappears when this.checkBullets/Stones added.
     // 2.6: Prevent infinite shooting.
 
     start() {
@@ -18,7 +15,7 @@ export class FallingStones extends GameTemplate {
         this.bullets = [];
         this.bulletSpeed;
         this.stones = [];
-        this.stoneSpeed = 3;
+        this.stoneSpeed = 1;
         this.stoneSpawnModifier = 0.2; //Determines how long it takes for new stones to appear (percent of screen).
     } 
 
@@ -26,106 +23,115 @@ export class FallingStones extends GameTemplate {
         this.inputBinding = {
             "left": this.player.left.bind(this.player),
             "right": this.player.right.bind(this.player),
-            //"up": this.generateBullet() //Causes bug.
+            "up": this.generateBullet()
         };
     }
 
     update(ctx) {
-        this.player.update(ctx)
-        this.bullets.forEach(bullet => bullet.update(ctx));
-        //this.checkBullets(ctx);
+        this.player.update(ctx);
+        this.generateStone(ctx);
+        //this.checkBullets(ctx); //buggy
         //this.checkStones(ctx);
         //this.gameOver();
     }
 
     draw(ctx) {
+        for(let i = 0; i < this.bullets.length; i++) {
+            this.bullets[i].draw(ctx);
+        }
         this.player.draw(ctx);
-        this.bullets.forEach(bullet => bullet.draw(ctx));
-        this.stones.forEach(stone => stone.draw(ctx));
         this.displayLives(ctx);
+        for(let j = 0; j < this.stones.length; j++) {
+            this.stones[j].draw(ctx);
+        }  
     }
 
     displayLives(ctx) {
         ctx.fillStyle = "#AEEDBD";
-        ctx.font = "40px Arial"
+        ctx.font = "40px Arial";
         ctx.textAlign = "center";
         ctx.fillText(this.lives, this.player.x + this.player.width/2, this.player.y + this.player.height - 10);
     }
 
     
     generateBullet() {
-        this.bullets.push(new Bullet(this.player));
+        this.bullets.push(new Bullet(this.player, this.bulletSpeed));
     }
 
+    deleteBullet(index) {
+        this.bullets.splice(index, 1);
+    }
     
     generateStone(ctx) {
         // Add new stone when 
         // 1) there is no stone yet 
         // 2) the last added stone has passed a certain percentage of the screen.
-        if(this.stones.length === 0 || (this.stones.length !== 0 && this.stones[this.stones.length - 1].y >= ctx.canvas.height * this.stoneSpawnModifier)) { 
+        if(this.stones.length == 0 || (this.stones.length != 0 && this.stones[this.stones.length - 1].y >= ctx.canvas.height * this.stoneSpawnModifier)) { 
             this.stones.push(new Stone(Math.random() * ctx.canvas.height), this.stoneSpeed);
         }
     }
 
-    
+    deleteStone(index) {
+        this.stones.splice(index, 1);
+    }
+
     checkBullets(ctx) {
         //Update
-        this.bullets.forEach(bullet => {
-            bullet.update(ctx);
-        });
-
+        for(let i = 0; i < this.bullets.length; i++) {
+            this.bullets[i].update(ctx);
+        }
         
         //Border
-        this.bullets.forEach(bullet => {
-            if(bullet.borderReached(ctx)) {
-                //remove bullet?
+        for(let i = 0; i < this.bullets.length; i++) {
+            if(this.bullets[i].borderReached(ctx)) {
+                this.deleteBullet(i);
             }
-        });
+        }
     }
 
     checkStones(ctx) {
         //Update
-        this.stones.forEach(stone => {
-            stone.update(ctx);
-        });
-
+        for(let j = 0; j < this.stones.length; j++) {
+            this.stones[j].update(ctx);
+        }
+        
         //Border
-        this.bullets.forEach(stone => {
-            if(stone.borderReached(ctx)) {
+        for(let i = 0; i < this.stones.length; i++) {
+            if(this.stones[i].borderReached(ctx)) {
                 this.lives--;
-                if(this.lives <= 0) this.gameOver();
+                if(this.lives === 0) this.gameOver = true;
             }
-            if(stone.borderPassed(ctx)) {
-                //remove stone? 
+        }
+        
+        for(let i = 0; i < this.stones.length; i++) {
+            if(this.stones[i].borderPassed(ctx)) {
+                this.deleteStone(i);
             }
-        });
+        }
 
         //Hit
-        this.stones.forEach(stone => {
-            if(stone.successfulHit(ctx)) {
-                this.points += 10;
-                //remove bullet?
-                //remove stone? 
+        for(let i = 0; i < this.stones.length; i++) {
+            for(let j = 0; j < this.bullets.length; j++) {
+                if(GameObject.rectangleCollision(this.stones[i], this.bullets[j])) {
+                    this.points += 1;
+                    this.deleteStone(i);                    
+                    this.deleteBullet(j);
+                }
             }
-        }); 
-    }
-
-    successfulHit(ctx) {
-        this.stones.forEach(bullet => {
-            if(false) return true; //check bullet-stone rectangle collision
-        });
+        }
     }
 
     gameOver() {
-        this.gameOver = true;
-        this.gameOverText = [
-            "GAME OVER", 
-            "Score: " + this.points,
-            "rematch: A"];
+        if(this.gameOver) {
+            this.gameOverText = [
+                "GAME OVER", 
+                "Score: " + this.points,
+                "rematch: A"];
+        }
     }
 
     static get NAME() {
-        return "FallingStones";
+        return "Falling Stones";
     }   
 }
 
@@ -159,9 +165,8 @@ export class Player extends MovableGameObject {
 
 export class Bullet extends MovableGameObject {
     
-    constructor(player) {
-        super(player.x + player.width/2, player.y + player.height/2, 10, 10, "#6bd26b", 0, -this.bulletSpeed);
-        this.speed = 20;
+    constructor(player, bulletSpeed) {
+        super(player.x + player.width/2, player.y + player.height/2, 10, 10, "#6bd26b", player.speed, -bulletSpeed);
     }
 
     borderPassed(ctx) {
@@ -176,13 +181,14 @@ export class Stone extends MovableGameObject {
         super(x, 0, 50, 100, "#6bd26b", 0, stoneSpeed);
     }
     
-    //Stone has reached border -> life lost. 
+    //Stone has reached border -> check for life lost. 
     borderReached(ctx) {
         return this.y > ctx.canvas.height;
     }
     
-    //Stone has fully passed border -> delete stone.
+    //Stone has fully passed border -> check for deleting a stone.
     borderPassed(ctx) {
         return this.y > this.height + ctx.canvas.height;
     }
 }
+
